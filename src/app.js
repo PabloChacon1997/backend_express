97bc703adc014a1bde201594e4fd52c8daf78875
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-02-21 11:37:25 -0500

    add instructions

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.listen(PORT, () => {
